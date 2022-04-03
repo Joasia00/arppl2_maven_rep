354eeeb0f2f731d1e5adf7b897ae007ac9f89827
Joasia00 <Asiawegiel123@wp.pl>
2022-04-03 13:53:59 +0200

    Pierwszy project maven

---
+++ pierwszy_projekt_maven/src/main/java/pl/sda/arppl2/maven/Main.java
@@ -0,0 +1,10 @@
+package pl.sda.arppl2.maven;
+
+public class Main {
+    // Generuje metodę startową:
+    // psvm - public static void main
+    public static void main(String[] args) {
+        //Generuje komende wypisywania do konsoli
+        System.out.println("Hello World!");
+    }
+}
